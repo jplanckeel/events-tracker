aa01129cb9c2b8feac47751a9b93c502bfd99611
Jeremy PLANCKEEL <jeremy.planckeel@outlook.com>
2024-01-21 13:38:13 +0100

    feat: add config for db and tls option
    Signed-off-by: Jeremy PLANCKEEL <jeremy.planckeel@outlook.com>

--- pkg/apis/event/v1/client.go
+++ pkg/apis/event/v1/client.go
@@ -2,8 +2,12 @@ package event
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"os"
 
+	"github.com/jplanckeel/events-tracker/pkg/config"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -19,21 +23,67 @@ func (c *EventsV1Client) EventsTracker(collection string) EventInterface {
 
 func NewClient() EventsV1Client {
 
-	host := "127.0.0.1"
-	port := 27017
-	//caCertPath := "<instance_certificate.crt>"
-	// prepare the uri for the connection
-	uri := fmt.Sprintf(
-		"mongodb://%s:%d",
-		host,
-		port,
-	)
+	var uri string
+	c := config.Config
 
+	// prepare the uri for the connection
+	if c.DatabaseUsername != "" && c.DatabasePassword != "" {
+		uri = fmt.Sprintf(
+			"mongodb://%s:%s@%s:%s/%s?maxPoolSize=20&?authMechanism=PLAIN",
+			c.DatabaseUsername,
+			c.DatabasePassword,
+			c.Database,
+			c.DatabasePort,
+			c.DatabaseName,
+		)
+	} else {
+		uri = fmt.Sprintf(
+			"mongodb://%s:%s/%s?maxPoolSize=20",
+			c.Database,
+			c.DatabasePort,
+			c.DatabaseName,
+		)
+	}
+
+	var caCertPool *x509.CertPool
+	var cert tls.Certificate
+	var err error
+
+	// Loads CA certificate file
+	if c.DatabaseCAFile != "" {
+		caCert, err := os.ReadFile(c.DatabaseCAFile)
+		if err != nil {
+			panic(err)
+		}
+		caCertPool = x509.NewCertPool()
+		if ok := caCertPool.AppendCertsFromPEM(caCert); !ok {
+			panic("Error: CA file must be in PEM format")
+		}
+	}
+
+	// Loads client certificate files
+	if c.DatabaseCertFile != "" && c.DatabaseKeyFile != "" {
+		cert, err = tls.LoadX509KeyPair(c.DatabaseCertFile, c.DatabaseKeyFile)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	var m *mongo.Client
 	ctx := context.Background()
-	m, _ := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if c.DatabaseCAFile != "" {
+
+		tlsConfig := &tls.Config{
+			RootCAs:      caCertPool,
+			Certificates: []tls.Certificate{cert},
+		}
+		m, _ = mongo.Connect(ctx, options.Client().ApplyURI(uri).SetTLSConfig(tlsConfig))
+	} else {
+		m, _ = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	}
 
 	var cs EventsV1Client
-	db := m.Database("ferretdb")
+	db := m.Database(c.DatabaseName)
 	db.CreateCollection(ctx, "events")
 	cs.mongoCollection = db.Collection("events")
 
--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -7,7 +7,9 @@ import (
 )
 
 func TestDefaultConfigValues(t *testing.T) {
-
+	assert.Equal(t, Config.Database, "127.0.0.1")
+	assert.Equal(t, Config.DatabaseName, "eventstracker")
+	assert.Equal(t, Config.DatabasePort, "27017")
+	assert.Equal(t, Config.LogLevel, "info")
 	assert.Equal(t, Config.Port, "9101")
-	assert.Equal(t, Config.LabelProject, "project")
 }
